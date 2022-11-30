91229057cffd7b14497d5e1cc3403f6ee2aa2cd0
WinnerRey <64565872+hasansaid@users.noreply.github.com>
2022-11-30 19:59:07 +0300

    navi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,16 @@ import { AppComponent } from './app.component';
 import { NaviComponent } from './component/navi/navi.component';
 import { ApplicantListComponent } from './component/applicant/applicant-list/applicant-list.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [AppComponent, NaviComponent, ApplicantListComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/models/token/tokenModel.ts
@@ -0,0 +1,4 @@
+export interface ITokenModel {
+  token: string;
+  expiration: string;
+}
