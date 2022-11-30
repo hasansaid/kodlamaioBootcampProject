7c4c080bb3351842551b347cb58b26094880a48c
WinnerRey <64565872+hasansaid@users.noreply.github.com>
2022-11-30 16:47:55 +0300

    bootstrap add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NaviComponent } from './component/navi/navi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NaviComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/component/navi/navi.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NaviComponent } from './navi.component';
+
+describe('NaviComponent', () => {
+  let component: NaviComponent;
+  let fixture: ComponentFixture<NaviComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NaviComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(NaviComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
