a68380eafa310c194801bc1747cf453a0cc6c3fb
WinnerRey <64565872+hasansaid@users.noreply.github.com>
2022-11-30 13:36:51 +0300

    arrangement

--- src/app/models/request/aplication/createAplicationRequest.ts
+++
@@ -1,9 +0,0 @@
-export interface ICreateApplicantRequest {
-  firstName: string;
-  lastName: string;
-  email: string;
-  password: string;
-  nationalIdentity: string;
-  dateOfBirth: Date;
-  about: string;
-}
