e2f3668d139617fadd1c24244a5c189f5db7fb1b
Uğur Meydanlı <ugurmeydanli089@gmail.com>
2022-12-03 19:01:37 +0300

    home page arangement

--- src/app/component/home/home-page/home-page.component.ts
+++ src/app/component/home/home-page/home-page.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+  styleUrls: ['./home-page.component.css'],
 })
 export class HomePageComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
