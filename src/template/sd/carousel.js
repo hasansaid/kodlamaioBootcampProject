36bd5f7ae9625cfbfc1048c67d86102b9866aa6d
Uğur Meydanlı <ugurmeydanli089@gmail.com>
2022-12-05 19:40:47 +0300

    arrangement

---
+++ src/template/sd/carousel.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  $(".testimonial .indicators li").click(function () {
+    var i = $(this).index();
+    var targetElement = $(".testimonial .tabs li");
+    targetElement.eq(i).addClass("active");
+    targetElement.not(targetElement[i]).removeClass("active");
+  });
+  $(".testimonial .tabs li").click(function () {
+    var targetElement = $(".testimonial .tabs li");
+    targetElement.addClass("active");
+    targetElement.not($(this)).removeClass("active");
+  });
+});
+$(document).ready(function () {
+  $(".slider .swiper-pagination span").each(function (i) {
+    $(this)
+      .text(i + 1)
+      .prepend("0");
+  });
+});
