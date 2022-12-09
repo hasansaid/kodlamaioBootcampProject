f97b9f2aa898dd2854a720d768609eda8cd293ac
Uğur Meydanlı <ugurmeydanli089@gmail.com>
2022-12-09 19:52:48 +0300

    arrangement

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { RegisterComponent } from './component/register/register.component';
 import { HomeBootcampDetailComponent } from './component/home/home-bootcamp-detail/home-bootcamp-detail/home-bootcamp-detail.component';
 import { LoginApplicantGuard } from './guards/applicantGuard/login-applicant.guard';
 import { ApplicantNotificationComponent } from './component/userApplicant/applicant-notification/applicant-notification.component';
@@ -59,6 +60,7 @@ const routes: Routes = [
       { path: 'home-about', component: HomeAboutComponent },
       { path: 'home-contact', component: HomeContactComponent },
       { path: 'home-login-employee', component: EmployeeLoginComponent },
+      { path: 'home-register', component: RegisterComponent },
       { path: 'home-login-instructor', component: InstructorLoginComponent },
       {
         path: 'home-login-employee/home-login-instructor',
---
+++ src/app/models/token/users-model.ts
@@ -0,0 +1,10 @@
+export interface GetUsers {
+  id: number;
+  name: string;
+  lastName: string;
+  email: string;
+  password: string;
+  expiration: string;
+  token: string;
+  role: string;
+}
