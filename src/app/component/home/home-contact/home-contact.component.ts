9b6417babc5f7be0e4ee07292a4a2d14705ba09f
WinnerRey <64565872+hasansaid@users.noreply.github.com>
2022-12-10 19:12:34 +0300

    arrangement

--- src/app/component/home/home-contact/home-contact.component.ts
+++ src/app/component/home/home-contact/home-contact.component.ts
@@ -13,5 +13,8 @@ export class HomeContactComponent implements OnInit {
 
   formSend() {
     this.toastrService.success('Mesajınız Başarıyla Gönderilmiştir.');
+    setTimeout(() => {
+      window.location.reload();
+    }, 1000);
   }
 }
